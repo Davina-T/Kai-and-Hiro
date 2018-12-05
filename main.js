09723259c871d6236a9cbc0d246d203988f5b7ab
Davina <36658212+Davina-T@users.noreply.github.com>
2018-12-05 18:09:48 +1000

    added product gallery, scrolling navbar effect, responsiveness

---
+++ main.js
@@ -0,0 +1,9 @@
+// Scrolling Effect
+
+$(window).on("scroll", function() {
+    if($(window).scrollTop()) {
+        $('nav').addClass('colour');
+    } else {
+        $('nav').removeClass('colour');
+    }
+})
